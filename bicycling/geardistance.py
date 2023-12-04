a0e5f0445fce6f5b67a6819d911d45d3dc1dfec7
rexdannan <122844381+rexdannan@users.noreply.github.com>
2023-12-03 20:47:31 -0700

    Create geardistance.py

---
+++ bicycling/geardistance.py
@@ -0,0 +1,13 @@
+import argparse
+import math
+import re
+import sys
+from wheelsize import Wheel, UserInput
+
+class GDUserInput(UserInput):
+    def __init__(self):
+        pass
+
+class GearDistance(Wheel):
+    def __init__(self):
+        pass
