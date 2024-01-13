80493f458b4f02b9c1afdad2caa39622a17f7b4a
rexdannan <122844381+rexdannan@users.noreply.github.com>
2024-01-12 21:00:32 -0700

    Create ftpact.py

---
+++ bicycling/ftpact.py
@@ -0,0 +1,42 @@
+# This utility will estimate a person's actual ftp based on wattage and time
+# Formula used: wattage * factor based on duration of test
+
+import sys
+import argparse
+
+class FtpAct
+
+    # Constants
+    HELP_TEXT = """
+    This utility calculates a person's ftp based on wattage output and test duration.
+    
+    Syntax: ftpact.py <watts> <durationinminutes>
+
+    Examples:
+    ftpact.py 270 3 - 270 watts, 3 minutes
+    ftpact.py 220 60 - 220 watts, 60 minutes
+    """
+
+    # Definition data
+    time_factor = { 1:.7, 3:.8, 5:.85, 20:.95, 60:1}
+
+    def __init__(self):
+        self.avg_watts=0
+        self.duration=0
+        self.duration_factor
+
+        # Parameters
+        self.parser = argparse.ArgumentParser(description=self.HELP_TEXT)
+        self.parser.add_argument('avg_watts', type=str, help='Average watts achieved during the test')
+        self.parser.add_argument('duration', type=str, help='Duration of the test expressed in minutes')
+    
+    def ProcessInput(self):
+        pass
+
+    def CalcFTPAct(self):
+        pass
+
+if __name__ == '__main__':
+    ftp = FtpAct()
+    ftp.ProcessInput()
+    print(ftp.CalcFTPAct())
